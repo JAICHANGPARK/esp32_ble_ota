81f33ae5f448b133ebd989f41de39478ecfffd3b
Dreamwalker <aristojeff@gmail.com>
2021-05-02 17:11:26 +0900

    Update main_app.ino

--- main_app/main_app.ino
+++ main_app/main_app.ino
@@ -34,35 +34,75 @@ uint8_t *int_array ;
 int buff_counter = 0;
 int chunk_counter = 0;
 
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-    Serial.printf("Listing directory: %s\n", dirname);
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels) {
+  Serial.printf("Listing directory: %s\n", dirname);
 
-    File root = fs.open(dirname);
-    if(!root){
-        Serial.println("Failed to open directory");
-        return;
-    }
-    if(!root.isDirectory()){
-        Serial.println("Not a directory");
-        return;
-    }
+  File root = fs.open(dirname);
+  if (!root) {
+    Serial.println("Failed to open directory");
+    return;
+  }
+  if (!root.isDirectory()) {
+    Serial.println("Not a directory");
+    return;
+  }
 
-    File file = root.openNextFile();
-    while(file){
-        if(file.isDirectory()){
-            Serial.print("  DIR : ");
-            Serial.println(file.name());
-            if(levels){
-                listDir(fs, file.path(), levels -1);
-            }
-        } else {
-            Serial.print("  FILE: ");
-            Serial.print(file.name());
-            Serial.print("  SIZE: ");
-            Serial.println(file.size());
-        }
-        file = root.openNextFile();
+  File file = root.openNextFile();
+  while (file) {
+    if (file.isDirectory()) {
+      Serial.print("  DIR : ");
+      Serial.println(file.name());
+      if (levels) {
+        listDir(fs, file.path(), levels - 1);
+      }
+    } else {
+      Serial.print("  FILE: ");
+      Serial.print(file.name());
+      Serial.print("  SIZE: ");
+      Serial.println(file.size());
     }
+    file = root.openNextFile();
+  }
+}
+
+void deleteFile(fs::FS &fs, const char * path) {
+  Serial.printf("Deleting file: %s\n", path);
+  if (fs.remove(path)) {
+    Serial.println("File deleted");
+  } else {
+    Serial.println("Delete failed");
+  }
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+void appendFileV2(fs::FS &fs, const char * path) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  for (int i = 0; i < n_elements; i++) {
+    Serial.println(file.write(int_array[i]));
+  }
+
+  file.close();
 }
 
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -83,26 +123,26 @@ class MyCallbacks: public BLECharacteristicCallbacks {
         Serial.println(rxValue.length());
         //        Serial.println("*********");
         //        Serial.print("Received Value: ");
-        
+
         for (int i = 0; i < rxValue.length(); i++)
         {
           bleFileBuff[i] = (uint8_t)rxValue[i];
           //          Serial.print(rxValue[i]);
           received_counter = received_counter + 1;
         }
-         Serial.print(">>> received_counter: ");
-         Serial.println(received_counter);
-        if(received_counter == 512){
+        Serial.print(">>> received_counter: ");
+        Serial.println(received_counter);
+        if (received_counter == 512) {
           Serial.println("received_counter is 512");
           isReceived = true;
-        }else{
+        } else {
           Serial.print(">>> chunk_counter: ");
-         Serial.println(chunk_counter);
-          if(chunk_counter == n_chunk_length -1){
+          Serial.println(chunk_counter);
+          if (chunk_counter == n_chunk_length - 1) {
             isReceived = true;
           }
         }
-        
+
 
         //        Serial.println();
         //        Serial.println("*********");
@@ -115,28 +155,28 @@ class OtaControlCallbacks: public BLECharacteristicCallbacks {
       std::string rxValue = pCharacteristic->getValue();
 
       if (rxValue.length() > 0) {
-             Serial.println("OtaControlCallbacks *********");
+        Serial.println("OtaControlCallbacks *********");
         for (int i = 0; i < rxValue.length(); i++)
         {
-                   Serial.print(rxValue[i]);
+          Serial.print(rxValue[i]);
         }
 
-      
+
         n_elements = ((uint8_t)rxValue[0] << 24) & 0xFF000000
-        | ((uint8_t)rxValue[1] << 16) & 0x00FF0000
-        | ((uint8_t)rxValue[2] << 8) & 0x0000FF00
-        | ((uint8_t)rxValue[3]) & 0x000000FF;
+                     | ((uint8_t)rxValue[1] << 16) & 0x00FF0000
+                     | ((uint8_t)rxValue[2] << 8) & 0x0000FF00
+                     | ((uint8_t)rxValue[3]) & 0x000000FF;
 
         n_chunk_length = ((uint8_t)rxValue[4] << 24) & 0xFF000000
-        | ((uint8_t)rxValue[5] << 16) & 0x00FF0000
-        | ((uint8_t)rxValue[6] << 8) & 0x0000FF00
-        | ((uint8_t)rxValue[7]) & 0x000000FF;
+                         | ((uint8_t)rxValue[5] << 16) & 0x00FF0000
+                         | ((uint8_t)rxValue[6] << 8) & 0x0000FF00
+                         | ((uint8_t)rxValue[7]) & 0x000000FF;
 
         Serial.print(">>> n_chunk_length:: ");
         Serial.println(n_chunk_length);
 
         Serial.println();
-          isPsramSetting = true;
+        isPsramSetting = true;
       }
     }
 };
@@ -146,32 +186,33 @@ void setup() {
   Serial.begin(115200);
   pinMode(5, OUTPUT);
 
-   if(!SD.begin(4)){
-        Serial.println("Card Mount Failed");
-        return;
-    }
-    uint8_t cardType = SD.cardType();
+  if (!SD.begin(4)) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
 
-    if(cardType == CARD_NONE){
-        Serial.println("No SD card attached");
-        return;
-    }
+  if (cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    return;
+  }
 
-    Serial.print("SD Card Type: ");
-    if(cardType == CARD_MMC){
-        Serial.println("MMC");
-    } else if(cardType == CARD_SD){
-        Serial.println("SDSC");
-    } else if(cardType == CARD_SDHC){
-        Serial.println("SDHC");
-    } else {
-        Serial.println("UNKNOWN");
-    }
+  Serial.print("SD Card Type: ");
+  if (cardType == CARD_MMC) {
+    Serial.println("MMC");
+  } else if (cardType == CARD_SD) {
+    Serial.println("SDSC");
+  } else if (cardType == CARD_SDHC) {
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
 
-    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
 
-listDir(SD, "/", 0);
+  listDir(SD, "/", 0);
+  deleteFile(SD, "/update.bin");
 
   // Create the BLE Device
   BLEDevice::init("UART Service");
@@ -199,10 +240,10 @@ listDir(SD, "/", 0);
   pRxCharacteristic->setCallbacks(new MyCallbacks());
 
   pOtaControlCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID_OTA_CONTROL,
-      BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_READ
-   );
-   pOtaControlCharacteristic->setCallbacks(new OtaControlCallbacks());
+                                CHARACTERISTIC_UUID_OTA_CONTROL,
+                                BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_READ
+                              );
+  pOtaControlCharacteristic->setCallbacks(new OtaControlCallbacks());
 
   // Start the service
   pService->start();
@@ -221,33 +262,33 @@ void loop() {
     //        pTxCharacteristic->notify();
     //        txValue++;
     if (isReceived) {
-      
+
       Serial.println("*********");
       Serial.print("Received Value: ");
-      if(received_counter == 512){
+      if (received_counter == 512) {
         for (int i = 0; i < 512; i++) {
-        Serial.print(bleFileBuff[i], HEX);
-        Serial.print(" ");
-        int_array[buff_counter] = bleFileBuff[i];
-        buff_counter++;
+          Serial.print(bleFileBuff[i], HEX);
+          Serial.print(" ");
+          int_array[buff_counter] = bleFileBuff[i];
+          buff_counter++;
         }
         Serial.println();
         Serial.print("buff_counter: ");
         Serial.print(buff_counter);
         Serial.println(" *********");
         receivedIndicator = !receivedIndicator;
-        digitalWrite(5, receivedIndicator); 
+        digitalWrite(5, receivedIndicator);
 
         chunk_counter++;
         pTxCharacteristic->setValue(chunk_counter);
         pTxCharacteristic->notify();
 
-        if(buff_counter == n_elements){
+        if (buff_counter == n_elements) {
           Serial.println(">>>>>>>>>>>>> Same? >>>>>>>>>>>>>>>>>>>>");
         }
         received_counter = 0;
-      }else{
-        if(chunk_counter == n_chunk_length -1){
+      } else {
+        if (chunk_counter == n_chunk_length - 1) {
           Serial.println(">>>>>>>>>>>>>>> is Last Packet");
           for (int i = 0; i < received_counter; i++) {
             Serial.print(bleFileBuff[i], HEX);
@@ -262,26 +303,28 @@ void loop() {
           chunk_counter++;
           pTxCharacteristic->setValue(chunk_counter);
           pTxCharacteristic->notify();
+          appendFileV2(SD, "/update.bin");
+
         }
       }
-       isReceived = false;
+      isReceived = false;
     }
-   
-    if(isPsramSetting){
+
+    if (isPsramSetting) {
       Serial.print("n_elements: ");
       Serial.println(n_elements);
       int temp = n_elements + 1024;
-      if(n_elements != 0){
+      if (n_elements != 0) {
         //psram setting
-       int_array = (uint8_t *) ps_malloc(temp * sizeof(uint8_t));
-      
-       Serial.println("Set PSRAM ps_malloc");
-      
-      }else{
-       //psram free 
-       free(int_array);
+        int_array = (uint8_t *) ps_malloc(temp * sizeof(uint8_t));
+
+        Serial.println("Set PSRAM ps_malloc");
+
+      } else {
+        //psram free
+        free(int_array);
         Serial.println("free PSRAM ");
-      
+
       }
       isPsramSetting = false;
     }
@@ -296,22 +339,22 @@ void loop() {
     pServer->startAdvertising(); // restart advertising
     Serial.println("start advertising");
     oldDeviceConnected = deviceConnected;
-     free(int_array);
-      n_elements = 0;
-      for(int i = 0; i < 1024; i++){
-        bleFileBuff[i] = 0x00;
-      }
-    
-      isReceived = false;
-      receivedIndicator = false;
-      isPsramSetting = false;
-      n_elements = 0;
-      n_chunk_length = 0;
-
-
-      buff_counter = 0;
-      chunk_counter = 0;
+    free(int_array);
+    n_elements = 0;
+    for (int i = 0; i < 1024; i++) {
+      bleFileBuff[i] = 0x00;
     }
+
+    isReceived = false;
+    receivedIndicator = false;
+    isPsramSetting = false;
+    n_elements = 0;
+    n_chunk_length = 0;
+
+
+    buff_counter = 0;
+    chunk_counter = 0;
+  }
   // connecting
   if (deviceConnected && !oldDeviceConnected) {
     // do stuff here on connecting
